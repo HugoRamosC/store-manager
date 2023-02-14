090df179216fd0913a076c0d669b932607deba1d
HugoRamosC <hugor.castilho@gmail.com>
2023-02-14 14:59:05 -0300

    feat: switch case for diferents https errors on validateName

--- src/services/validations/productsValidations.js
+++ src/services/validations/productsValidations.js
@@ -9,9 +9,14 @@ const validateId = (id) => {
 
 const validateName = (name) => {
   const { error } = nameSchema.validate(name);
-  if (error) return { status: 'BAD_REQUEST_STATUS', message: error.message };
-
-  return { status: null, message: '' };
+  switch (error.details[0].type) {
+    case 'any.required':
+        return { status: 'BAD_REQUEST_STATUS', message: error.message };
+    case 'string.min':
+      return { status: 'INVALID_VALUE', message: error.message };
+    default:
+      return { status: null, message: '' };
+  }
 };
 
 module.exports = {
--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -3,8 +3,12 @@ const Joi = require('joi');
 const idSchema = Joi.number().integer().min(1).required()
   .label('ID');
 
-const nameSchema = Joi.string().min(3).max(30).required()
-  .label('NAME');
+const nameSchema = Joi.string().min(5).max(30).required()
+  .label('name')
+  .messages({
+    'string.min': '{#label} length must be at least {#limit} characters long',
+    'string.base': '{#label} is required',
+  });
 
 module.exports = {
   idSchema,
